e1509e7995790c433241a3c9a70c1667695b9a14
AngelGa8 <lechep8@gmail.com>
2024-08-02 19:32:03 -0600

    Actualizacion Tareas

---
+++ Tareas/Tarea_1.js
@@ -0,0 +1,4 @@
+var numero=parseInt(prompt("Dame un valor para sumar: ","Sumando"));
+var random= Math.trunc(Math.random() * 10);
+var resultado= numero+random;
+document.write("La suma de "+numero+" + "+random+" es = "+resultado+"<br/>");
